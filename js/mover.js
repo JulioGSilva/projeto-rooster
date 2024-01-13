d1651b54cb924a8d36780055426516bcb9934a70
Júlio Gabriel <113954423+JulioGSilva@users.noreply.github.com>
2024-01-13 12:10:44 -0300

    att

--- js/mover.js
+++ js/mover.js
@@ -1,38 +1,37 @@
 function moveR() {
     const roosterDiv = document.querySelector('.rooster');
     roosterDiv.scrollLeft += 410;
+
 }
 
 function moveL() {
     const roosterDiv = document.querySelector('.rooster');
     roosterDiv.scrollLeft -= 410;
+
 }
 
 function detalhes() {
     var show = document.getElementById('descricao');
-    if (show.style.display === 'none') {
-        show.style.display="block";
-    } else {
-        show.style.display='none';
-    }
-    
+    show.style.display = "block"
+
 }
 
-function more(divElement) {
-    let img = divElement.querySelector('img').src;
-    alert(img)
-    
+function voltar() {
+    var show = document.getElementById('descricao');
+    show.style.display = "none";
+
 }
 
-function pegarSrc(divElement) {
-    // Encontre o elemento img dentro do elemento div clicado
-    const imgElement = divElement.querySelector('img');
-    
+function pegarSrc(idImagem) {
+    console.log(idImagem)
+    let imgElement = document.getElementById(idImagem)
+    console.log(imgElement)
     if (imgElement) {
-        // Acesse o atributo src da imagem
-        const srcDaImagem = imgElement.src;
-        
-        // Faça o que desejar com o srcDaImagem, como exibi-lo em um alert
-        alert('O src da imagem clicada é: ' + srcDaImagem);
+        let srcDaImagem = imgElement.src;
+        let att = document.getElementById('foto');
+        att.src = srcDaImagem
+    } else {
+        alert('erro')
     }
+
 }
